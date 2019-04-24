e5bb830fd3365e3f737f3ff50d05cd033348be98
Chris Kay <chriskay25@gmail.com>
2019-04-24 18:33:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(deli)
   if deli.length == 0 
     puts "The line is currently empty."
   else 
-    puts "The line is currently: " + line_position(deli)
+    puts "The line is currently: " + line_position(deli).to_s
   end
 end
 
