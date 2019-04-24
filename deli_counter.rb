89a3e8a0686a4bc54ed7eb411558cd684edc0e9c
Chris Kay <chriskay25@gmail.com>
2019-04-24 17:56:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,5 +4,10 @@ def line(deli)
   if deli.length == 0 
     puts "The line is currently empty"
   else 
-    puts "The line is currently"
+    puts "The line is currently: "
+  end
 end
+
+def take_a_number(deli, new_customer)
+  puts "#{new_customer}"
+end 
